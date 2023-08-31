44afd03a162c4c76e6a964fe7fc661c36c6a2448
doki <h18673628658@outlook.com>
2023-08-31 14:16:22 +0800

    fix: 添加vite配置

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { prismjsPlugin } from 'vite-plugin-prismjs'
+import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    prismjsPlugin({
+      languages: ['go', 'shell', 'java', 'javascript', 'rust', 'c', 'cpp', 'css', 'json', 'sql', 'c#', 'git', 'lua', 'yaml', 'docker', 'typescript', 'html', 'xml'],
+    }),
+    svgLoader(),
   ],
   resolve: {
     alias: {
